901a022aa198763a20c01545f840093fe3f1613b
Pawel Golian <pawel.golian@gmail.com>
2023-06-01 15:41:48 +0200

    Update configuration for installing Chromium only

--- pages/main.page.ts
+++ pages/main.page.ts
@@ -3,6 +3,6 @@ import { Page } from "@playwright/test";
 export class MainPage {
     constructor(private page: Page) { }
 
-    acceptTermsHeader = this.page.getByRole('button', { name: 'Accept all' })
-    acceptTermsButton = this.page.locator('#L2AGLb')
+    acceptTermsHeader = this.page.locator('#S3BnEe')
+    acceptTermsButton = this.page.getByRole('button', { name: 'Accept all' })
 }
