e014881715bb93d1a4edaabb8710d80a67f7437c
Pawel Golian <pawel.golian@gmail.com>
2023-06-01 15:57:28 +0200

    refactor: add new step in test

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -5,6 +5,7 @@ test('Visual Test - Google Homepage Layout', async ({ page }) => {
   const mainPage = new MainPage(page)
   
   await page.goto('/');
+  await page.waitForLoadState('networkidle')
   // await mainPage.acceptTermsButton.click()
   
   await expect(page).toHaveScreenshot('google-main-page.png');
