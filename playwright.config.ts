ceca46294ec93970f619819cf084b7cc85c2f093
Pawel Golian <pawel.golian@gmail.com>
2023-06-05 14:07:05 +0200

    add geolocation in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.google.com/',
     locale: 'en-GB',
+    geolocation: { longitude: 51.9194, latitude: 19.1451 },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
