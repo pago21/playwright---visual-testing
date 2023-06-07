16e04ac32a876dd0658ee122f3d96630b60997c2
Pawel Golian <pawel.golian@gmail.com>
2023-06-07 15:36:37 +0200

    add simply test for other project

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -3,8 +3,14 @@ import { MainPage } from '../pages/main.page';
 
 test('Visual Test - Google Homepage Layout', async ({ page }) => {
   const mainPage = new MainPage(page)
-  await page.waitForTimeout(5000)
   await page.goto('/');
   await mainPage.acceptTermsButton.click()
   await expect(page).toHaveScreenshot('google-main-page.png'); 
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://portalpacjenta.luxmed.pl/PatientPortal/NewPortal/Page/Account/Login');
+  await page.waitForLoadState('networkidle')
+  await expect(page).toHaveTitle('Portal Pacjenta LUX MED')
+  await page.locator('#Login').click();
 });
